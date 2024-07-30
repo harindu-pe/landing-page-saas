5852e77ef17187132a6f9e875d5f5761f122a29d
Harindu Egalla <harindu.e@asyncmetric.com>
2024-07-30 19:01:23 +0530

    pricing done

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import CheckIcon from "@/assets/check.svg";
+import { cn } from "@/lib/utils";
+
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +55,77 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">
+            {
+              "Free forever. Upgrade for unlimited tasks, better security, and exclusive features."
+            }
+          </p>
+        </div>
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              <div
+                className={cn(
+                  "p-10 border border-[$F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-xs w-full",
+                  { "border-black bg-black text-white": inverse === true }
+                )}
+              >
+                <div className="flex justify-between">
+                  <h3
+                    className={cn("text-lg font-bold text-black/50", {
+                      "text-white/60": inverse === true,
+                    })}
+                  >
+                    {title}
+                  </h3>
+                  {popular === true && (
+                    <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                        Popular
+                      </span>
+                    </div>
+                  )}
+                </div>
+                <div className="flex items-baseline gap-1 mt-[30px]">
+                  <span className="text-4xl font-bold tracking-tighter leading-0">
+                    ${monthlyPrice}
+                  </span>
+                  <span className="tracking-tight font-bold text-black/50">
+                    /month
+                  </span>
+                </div>
+                <Button
+                  className={cn("w-full mt-[30px]", {
+                    "bg-white text-black": inverse === true,
+                  })}
+                >
+                  {buttonText}
+                </Button>
+                <ul className="flex flex-col gap-5 mt-8">
+                  {features.map((feature) => (
+                    <li className="text-sm flex items-center gap-4">
+                      <CheckIcon className="size-6" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
